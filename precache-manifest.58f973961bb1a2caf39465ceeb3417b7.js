d60b562f01b897a3b5508058a6532619fd0db810
greenblue467 <greenblue467@hotmail.com>
2020-02-04 17:32:16 +0800

    Updates

---
+++ precache-manifest.58f973961bb1a2caf39465ceeb3417b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41be60e4ef50b0667976170c9df3ed93",
+    "url": "/p2-todolist/index.html"
+  },
+  {
+    "revision": "920f8dfcc851acac4591",
+    "url": "/p2-todolist/static/css/main.86b7540d.chunk.css"
+  },
+  {
+    "revision": "5458b2041899c2f36651",
+    "url": "/p2-todolist/static/js/2.a4a99496.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/p2-todolist/static/js/2.a4a99496.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "920f8dfcc851acac4591",
+    "url": "/p2-todolist/static/js/main.4dc98704.chunk.js"
+  },
+  {
+    "revision": "fad96f5f8da0f7824556",
+    "url": "/p2-todolist/static/js/runtime-main.4491c6f1.js"
+  }
+]);
